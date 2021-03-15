2bfafd9d7e06f464fbee14273f4da3de1d92a124
Aitor Bouzas <abouzas@bmat.com>
2021-03-15 19:15:45 +0100

    corregido listas 26

--- Listas/21-30/26.py
+++ Listas/21-30/26.py
@@ -1,28 +1,31 @@
 # Write a python program to check whether two lists are circularly identical.
 
+import timeit
+
+
 list_first = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] 
-list_second = [7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6] 
-
-# def circular_identity(list1, list2):
-#     if sorted(list_first) == sorted(list_second):
-#         return True
-
-# print(circular_identity(list_first, list_second)) 
+list_second = [7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
-def check_circ_identity(list, list2):
-    if (' '.join(str(list_first))) in (' '.join(str(list_first))) * 2:
+def check_circ_identity(lista, list2):
+    if ' '.join(map(str, lista)) in ' '.join(map(str, list2 * 2)):
         return True
+    else:
+        return False
 
-print(check_circ_identity(list_first, list_second))
 
+def check_circ_identity_2(lista, list2):
+    size = len(lista)
+    if size != len(list2):
+        return False
+    value = lista[0]
+    try:
+        index = list2.index(value)
+    except ValueError:
+        return False
 
-# g = (' '.join(str(list_first)))
+    ordered = [list2[i] if i < size else list2[i - size] for i in range(index, index + size)]
+    return ordered == lista
 
-# print(type(g))
-
-# g = (' '.join(map(str ,list_first)))
-
-# print(type(g))
-
-# print(g)
+print(timeit.timeit(lambda: check_circ_identity(list_first, list_second)))
+print(timeit.timeit(lambda: check_circ_identity_2(list_first, list_second)))
