76bf0ddc3b2c4661d227260baf8799df3be8d9a6
PabloLarrie <pablo_14_pont@hotmail.com>
2021-03-25 17:00:53 +0100

    dicts +25

--- Diccionarios/21-30/25.py
+++ Diccionarios/21-30/25.py
@@ -2,4 +2,9 @@
 
 my_dict = {"C1": [1, 2, 3], "C2": [5, 6, 7], "C3": [9, 10, 11]}
 
-print(my_dict[1])
+listed = list(my_dict.items())
+
+print(listed[0][0], listed[1][0], listed[2][0])
+print(listed[0][1][0], listed[0][1][1], listed[0][1][2])
+print(listed[1][1][0], listed[1][1][1], listed[1][1][2])
+print(listed[2][1][0], listed[2][1][1], listed[2][1][2])
